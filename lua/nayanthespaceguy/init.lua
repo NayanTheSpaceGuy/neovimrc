5c31674e2743f7fb83e800810826abe5786383e0
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-02-21 20:57:59 +0530

    The Actual Inital Commit
    Thanks to TheVimeagen for most of the stuff. Truly an inspiration. The name is... NayanTheVimGuy

---
+++ lua/nayanthespaceguy/init.lua
@@ -0,0 +1,41 @@
+require("nayanthespaceguy.set")
+require("nayanthespaceguy.remap")
+
+require("nayanthespaceguy.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local NayanTheSpaceGuyGroup = augroup('NayanTheSpaceGuy', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+vim.filetype.add({
+    extension = {
+        templ = 'templ',
+    }
+})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = NayanTheSpaceGuyGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
